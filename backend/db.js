60eca0855fe9b8a7d0e46010a67b775d657ebb58
ROHIT KUMAR <yrohit3260@gmail.com>
2026-04-27 23:05:38 +0530

    Add DB_PORT

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,8 @@ const db = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "smartspend",
+  port: process.env.DB_PORT || 3306,
+  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
